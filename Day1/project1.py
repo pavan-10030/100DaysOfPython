3e721f069ace3bef231d8ae50eb31255a962eb06
Bandike Pavan Kumar <srinivasbandik@gmail.com>
2026-04-05 23:07:05 +0530

    Add files via upload

---
+++ Day1/project1.py
@@ -0,0 +1,5 @@
+# Project 1 : Band Name Generator
+print("Welcome to the Band Name Generator")
+city = input("Which city did you grow up in?\n")
+petName = input("What is the name of a pet?\n")
+print("Your Band Name could be "+city+" "+petName)
---
+++ Day1/variables.py
@@ -0,0 +1,9 @@
+name = "Pavan Kumar"
+print(name)
+
+name = "Pavan"
+print(name)
+
+username = input("What is your name?")
+length = len(username)
+print(length)
